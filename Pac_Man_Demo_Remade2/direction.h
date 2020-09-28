a918638e0e12670a1c11cb4803d57027965a6933
broncoian2 <ian.reim@cuw.edu>
2020-09-28 16:03:00 -0500

    got rid of scrap
    made this off a template, forgot to clean up.

--- Pac_Man_Demo_Remade2/direction.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-enum spriteDirection
-{
-	up = 1,
-	down = -1,
-	left = 2,
-	right = -2,
-	unset = 0
-};
--- Pac_Man_Demo_Remade2/input.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-void movePac(sf::Sprite &pacManSprite);
